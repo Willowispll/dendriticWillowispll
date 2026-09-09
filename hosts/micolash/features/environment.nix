33c645556a07dbc3c441a1b446cebcff3c179f17
willowispll <gorelikgleb+github@gmail.com>
2026-09-09 03:17:38 +0300

    hosts/micolash: fix spelling

--- hosts/micolash/features/environment.nix
+++ hosts/micolash/features/environment.nix
@@ -2,7 +2,7 @@
   flake.hosts.micolash.features.environment = {pkgs, ...}: {
     environment.systemPackages = with pkgs; [
       alacritty
-      Waytrogen
+      waytrogen
       fuzzel
       brave-origin
       xdg-utils
